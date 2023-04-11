1ee764485fc991e0f0f69b38beeaf886b305aeaf
せんおよんと <yuntumg@gmail.com>
2023-04-11 13:03:40 +0900

    Update settings.py

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000", "https://nextjs-blog-todos.vercel.app"
+    "http://localhost:3000", "https://udemy-next-frontend.vercel.app"
 ]
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
